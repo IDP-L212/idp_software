48d8266a74eda7252a08442e9cd1628720bf6c6b
Lohith Konathala <lk480@cam.ac.uk>
2022-02-20 23:59:40 +0000

    removed header and src files for library due to errors in compiling

--- lent_212_library.cpp
+++
@@ -1,9 +0,0 @@
-//Main src file for all function implementations 
-// This syntax should be used for programmer defined header files
-
-#include "lent_212_library.h"
-//Example function to illustrate format
-int add_two_integers(int a, int b)
-{
-    return a + b;
-}
--- lent_212_library.h
+++
@@ -1,7 +0,0 @@
-//Header file for private libary containing all the functions needed to perform task
-#ifndef LENT_212_LIBRARY_H
-#define LENT_212_LIBRARY_H
-#include <Arduino.h>
-//Example function to illustrate format
-int add_two_integers(int a, int b);
-#endif
